83317268a472fa18bd2d83c22758bcdda976d6c7
Alexander Frederik Vreeken <saf.vreeken@gmail.com>
2017-11-07 20:08:04 +0100

    Created the first screen

--- CaféNinja/Controller/MainVC.swift
+++ CaféNinja/Controller/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  CaféNinja
 //
 //  Created by Alexander Frederik Vreeken on 07/11/2017.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
